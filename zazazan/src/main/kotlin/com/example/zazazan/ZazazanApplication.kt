5e789bb3ba65d3868f410fafbb51b9a63790fd49
jeongmyeong <kimbym23@gmail.com>
2021-12-20 08:16:59 +0900

    initialize commit

---
+++ zazazan/src/main/kotlin/com/example/zazazan/ZazazanApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zazazan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZazazanApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZazazanApplication>(*args)
+}
