f6bb139b31787abd2db8200bb89ca6459df0fc94
jeongmyeong <kimbym23@gmail.com>
2022-01-05 22:28:23 +0900

    add mvc test and fix 406 error

---
+++ zazazan/src/test/kotlin/com/example/zazazan/logger/presentation/ui/api/LoggingControllerTest.kt
@@ -0,0 +1,35 @@
+package com.example.zazazan.logger.presentation.ui.api
+
+import com.example.zazazan.logger.application.service.LoggingService
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@WebMvcTest(controllers = [LoggingController::class])
+@AutoConfigureMockMvc
+class LoggingControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var loggingService: LoggingService
+
+    @Test
+    fun writeLog() {
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/logs"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andDo(MockMvcResultHandlers.print())
+
+        verify(loggingService, times(1)).write()
+    }
+}
